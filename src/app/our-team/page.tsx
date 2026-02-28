fae15847da74b167442d8e65bc0637e3faa036d1
Bibek Moktan <Bibekmoktan1222@gmail.com>
2026-02-28 15:44:59 +0530

    frontend backend seperation

--- src/app/our-team/page.tsx
+++
@@ -1,5 +0,0 @@
-import OurTeam from '../components/OurTeam/OurTeam';
-
-export default function OurTeamPage() {
-  return <OurTeam />;
-} 
--- src/app/trekking/components/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Trekking Components Export Index
- * This file exports all components related to trekking functionality
- * for easier imports throughout the application
- */
-
-export { default as TrekkingPage } from './TrekkingPage'; 
